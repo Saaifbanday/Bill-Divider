ed37b842d43ad4260f74d27673d2973bc8be1752
Saaifbanday <saaifbanday136@gmail.com>
2024-09-11 22:17:01 +0530

    added

--- Toast-Pop_Up/script.js
+++ Toast-Pop_Up/script.js
@@ -7,65 +7,65 @@ const message = document.querySelector('.toast-message')
 const reset = document.querySelector('.reset')
 const own = document.querySelector('.own')
 
-showToastBtn.addEventListener('click',()=>{
+showToastBtn.addEventListener('click', () => {
 
     const newToast = document.createElement('div')
     newToast.classList.add("toast")
-    toastContainer.append(newToast)
     newToast.innerHTML = `✗ ${message.value}`;
+    toastContainer.append(newToast)
 
-   if(horizontalPosition.value === "right"){
-    toastContainer.classList.add('right')
-   }
-   else{
-    toastContainer.classList.remove('right')
-   }
-   if(verticalPosition.value === "bottom"){
-    toastContainer.classList.add('bottom')
-   }
-   else{
-    toastContainer.classList.remove('bottom')
-   }
-   if(toastType.value === "success"){
-      
-
-       newToast.innerHTML = `✓ ${message.value}`;
-         newToast.style.backgroundColor = "green"
-         const closeIcon = document.createElement('span')
-         closeIcon.innerText = " ✕"
-         newToast.append(closeIcon) 
-         closeIcon.classList.add('closeIcon')
-        
-   }
-   else if(toastType.value==="error"){
-    newToast.style.backgroundColor = "red"
     const closeIcon = document.createElement('span')
     closeIcon.innerText = " ✕"
-    newToast.append(closeIcon) 
     closeIcon.classList.add('closeIcon')
-   }
-   else if(toastType.value==="warning"){
-    newToast.style.backgroundColor = "crimson"
-    const closeIcon = document.createElement('span')
-    closeIcon.innerText = " ✕"
-    newToast.append(closeIcon) 
-    closeIcon.classList.add('closeIcon')
-   
-   }
-   else if(toastType.value==="info"){
-       newToast.style.backgroundColor = "orange"
-       const closeIcon = document.createElement('span')
-       closeIcon.innerText = " ✕"
-       newToast.append(closeIcon) 
-       closeIcon.classList.add('closeIcon')
-   }
+    newToast.append(closeIcon)
 
-   closeIcon.addEventListener('click',()=>{
-      newToast.style.display = "none"
-   })
-  
+    if (horizontalPosition.value === "right") {
+        toastContainer.classList.add('right')
+    }
+    else {
+        toastContainer.classList.remove('right')
+    }
+    if (verticalPosition.value === "bottom") {
+        toastContainer.classList.add('bottom')
+    }
+    else {
+        toastContainer.classList.remove('bottom')
+    }
+
+    if (toastType.value === "success") {
+        newToast.style.backgroundColor = "green"
+        //----Event Listner Added here
+        closeIcon.addEventListener('click', () => {
+            newToast.style.display = "none"
+        })
+
+    }
+    else if (toastType.value === "error") {
+        newToast.style.backgroundColor = "red"
+        //----Event Listner Added here
+        closeIcon.addEventListener('click', () => {
+            newToast.style.display = "none"
+        })
+    }
+    else if (toastType.value === "warning") {
+        newToast.style.backgroundColor = "crimson"
+    // Event Listner Added here
+        closeIcon.addEventListener('click', () => {
+            newToast.style.display = "none"
+        })
+
+    }
+    else if (toastType.value === "info") {
+        newToast.style.backgroundColor = "orange"
+    // Added Event Listner
+        closeIcon.addEventListener('click', () => {
+            newToast.style.display = "none"
+        })
+    }
+
+
 })
 
-reset.addEventListener('click',()=>{
-    toastContainer.innerText = " "  
+reset.addEventListener('click', () => {
+    toastContainer.innerText = " "
 })
